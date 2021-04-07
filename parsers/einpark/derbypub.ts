ffe93fe0e4d4bf0fe36c8c8bb53d4187eff3ab69
Edinson Gomez <32436116+EdisonyGomez@users.noreply.github.com>
2021-04-06 20:53:20 -0500

    Mejoras al sitio(checkbox)

---
+++ parsers/einpark/derbypub.ts
@@ -0,0 +1,55 @@
+import cheerio from "cheerio";
+
+import { IMenuItem } from "../IMenuItem";
+import { IParser } from "../IParser";
+import { format } from "date-fns";
+import { sk } from "date-fns/locale";
+
+export class DerbyPub implements IParser {
+    public parse(html: string, date: Date, doneCallback: (menu: IMenuItem[]) => void): void {
+        const $ = cheerio.load(html);
+        const dayMenu = new Array<IMenuItem>();
+        const todayText = format(date, "d. LLLL yyyy", { locale: sk });
+
+        $(".pm-date-sub").each((i, elem) => {
+            const node = $(elem);
+            const text = node.text().trim();
+            if (todayText === text) {
+                parseDailyMenu(node.nextAll(".pm-item").first().find("table"));
+                return false;
+            }
+        });
+
+        doneCallback(dayMenu);
+
+        function parseDailyMenu(table: cheerio.Cheerio) {
+            const rows = table.find("tr");
+            rows.each((index, elem) => {
+                if (index === 0) {
+                    dayMenu.push(parseSoup(elem));
+                } else {
+                    dayMenu.push(parseOther(elem));
+                }
+            });
+        }
+
+        function parseSoup(row: cheerio.Element): IMenuItem {
+            const cells = $(row).find("td");
+            const text = cells.eq(2).text().trim();
+            return { isSoup: true, text: normalize(text), price: NaN };
+        }
+
+        function parseOther(row: cheerio.Element): IMenuItem {
+            const cells = $(row).find("td");
+            const text = cells.eq(2).text().trim();
+            const price = parseFloat(cells.eq(4).text().replace(",", "."));
+            return { isSoup: false, text: normalize(text), price };
+        }
+
+        function normalize(str: string) {
+            return str.removeAlergens()
+                .removeMetrics()
+                .capitalizeFirstLetter();
+        }
+    }
+ }
